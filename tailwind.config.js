9e667e52db234234cca98648154f58ae95d0af8d
abdul534 <seercm@gmail.com>
2022-04-01 14:18:27 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000",
+      },
+    },
+  },
+  plugins: [require('tailwind-scrollbar-hide')],
+}
